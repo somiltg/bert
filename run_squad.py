f2a490761ef0e45959823be38aa09e62b56fe68d
ngk123 <khadenilesh92@gmail.com>
2020-04-12 19:15:21 -0400

    removed pdb

--- run_squad.py
+++ run_squad.py
@@ -260,8 +260,7 @@ def read_squad_examples(input_file, is_training):
         orig_answer_text = None
         is_impossible = False
         if is_training:
-          import pdb
-          pdb.set_trace()
+          print(FLAGS)
           
           if FLAGS.version_2_with_negative:
             is_impossible = qa["is_impossible"]
